a918d2c05d28415bf61f2aaa3f0f78b7eff89ac5
Artem Mahey <artemmagej@gmail.com>
2023-07-31 15:52:32 +0300

    feat: fullstack script

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
-# express-ts
+# Create
 
-A CLI to automatically clone the [Express template API with Typescript](https://github.com/JeriRov/express-ts-template).
+This CLI allows you to create a template for your project, whether it's an express, React, or fullstack application.
 
 ## Usage
-Create with Typescript:
+
+[Create express api with Typescript:](https://github.com/JeriRov/express-ts-template.git)
 
 ```sh
-npx @artemmahey/express-ts -d name-of-app
+npx @artemmahey/create -e name-of-app
+```
+
+[Create react app with Typescript:](https://github.com/JeriRov/react-ts-template)
+
+```sh
+npx @artemmahey/create -r name-of-app
+```
+
+[Create fullstack app with Typescript:]()
+
+```sh
+npx @artemmahey/create -r -e name-of-app
 ```
 
 This will create a directory with the given name, clone the chosen repo into it, and install dependencies.
 
 ## Additional
+
 This CLI is based on [create-express-api](https://github.com/w3cj/create-express-api)
--- index.js
+++ index.js
@@ -1,77 +1,124 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
-const { spawn } = require('child_process');
+const {spawn} = require('child_process');
 const degit = require('degit');
 const args = require('args');
 
 args
-  .option('directory', 'The name of the directory to create.')
-  .examples([{
-    usage: '@artemmahey/express-ts -d my-api',
-    description: 'Create express api in directory my-api',
-  }]);
+    .option('express', 'The name of the directory to create.')
+    .examples([{
+        usage: '@artemmahey/create --express my-api',
+        description: 'Create express api in directory my-api',
+    }])
+
+    .option('react', 'The name of the directory to create.')
+    .examples([{
+        usage: '@artemmahey/create --react my-app',
+        description: 'Create react app in directory my-app',
+    }, {
+        usage: '@artemmahey/create --express --react my-app',
+        description: 'Create fullstack app in directory my-app',
+    }]);
 
 const flags = args.parse(process.argv, {
-  name: 'express-ts-template',
+    name: '@artemmahey/create',
 });
 
-import('chalk').then(({ default: chalk }) => {
-  function errorLog(message) {
-    console.log(chalk.red.bgBlack(message));
-  }
+import('chalk').then(({default: chalk}) => {
+    function errorLog(message) {
+        if(message.includes('WARN')) {
+            console.log(chalk.yellow.bgBlack(message));
+            return;
+        }
+        console.log(chalk.red.bgBlack(message));
+    }
 
-  if (!flags.d || flags.d.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
-    errorLog(`Error: Missing or Invalid directory name: "${flags.d}"`);
-    args.showHelp();
-    process.exit(-1);
-  }
+    let express = 'e';
+    let react = 'r';
 
-  if (fs.existsSync(flags.d)) {
-    errorLog(`Error: Directory "${flags.d}" already exists.`);
-    process.exit(-1);
-  }
+    let dirName = '';
+    let repoName = '';
+    let howToStartMessage = '';
+    switch (!!flags) {
+        case flags.hasOwnProperty(express) && flags.hasOwnProperty(react):
+            dirName = typeof flags[express] === 'string' ? flags[express] : flags[react];
+            repoName = 'fullstack';
+            howToStartMessage = `cd backend \nnpm run dev \ncd ../frontend \nnpm start`
+            break;
+        case flags.hasOwnProperty(express):
+            dirName = flags[express]
+            repoName = 'JeriRov/express-ts-template.git';
+            howToStartMessage = `npm run dev`
+            break;
+        case flags.hasOwnProperty(react):
+            dirName = flags[react];
+            repoName = 'JeriRov/react-ts-template.git';
+            howToStartMessage = `npm start`
+            break;
+        default:
+            errorLog('Error: Please specify a template.');
+    }
 
-  const repoName = 'JeriRov/express-ts-template.git';
+    if (typeof dirName !== 'string' || RegExp(/[<>:"\/\\|?*\x00-\x1F]/).exec(dirName)) {
+        errorLog(`Error: Missing or Invalid directory name: "${dirName}"`);
+        args.showHelp();
+        process.exit(-1);
+    }
 
-  const emitter = degit(repoName, {
-    force: true,
-    verbose: true,
-  });
+    if (fs.existsSync(dirName)) {
+        errorLog(`Error: Directory "${dirName}" already exists.`);
+        process.exit(-1);
+    }
 
-  emitter.on('info', (info) => {
-    console.log(info.message);
-  });
+    function runCommand(command, args, options = undefined) {
+        const spawned = spawn(command, args, options);
+        return new Promise((resolve) => {
+            spawned.stdout.on('data', (data) => {
+                console.log(chalk.bgBlack.white(data.toString()));
+            });
 
-  function runCommand(command, args, options = undefined) {
-    const spawned = spawn(command, args, options);
+            spawned.stderr.on('data', (data) => {
+                errorLog(data.toString());
+            });
 
-    return new Promise((resolve) => {
-      spawned.stdout.on('data', (data) => {
-        console.log(chalk.bgBlack.white(data.toString()));
-      });
+            spawned.on('close', () => {
+                resolve();
+            });
+        });
+    }
 
-      spawned.stderr.on('data', (data) => {
-        errorLog(data.toString());
-      });
+    function cloneRepo(dirName, repoName, howToStartMessage) {
+        const emitter = degit(repoName, {
+            force: true,
+            verbose: true,
+        });
 
-      spawned.on('close', () => {
-        resolve();
-      });
-    });
-  }
+        emitter.on('info', (info) => {
+            console.log(info.message);
+        });
 
-  emitter.clone(flags.d).then(() => {
-    console.log(chalk.bgBlack.cyan('Installing dependencies...'));
-    const command = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
-    return runCommand(command, ['install'], {
-      cwd: `${process.cwd()}/${flags.d}`,
-    }).then(() => {
-      console.log(chalk.bgBlack.cyan('Done!'));
-      console.log('');
-      console.log(chalk.bgBlack.white('To get started:'));
-      console.log(chalk.bgBlack.cyan(`cd ${flags.d}`));
-      console.log(chalk.bgBlack.cyan('npm run dev'));
-    });
-  });
+        emitter.clone(dirName).then(() => {
+            console.log(chalk.bgBlack.cyan('Installing dependencies...'));
+            const command = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+            return runCommand(command, ['install'], {
+                cwd: `${process.cwd()}/${dirName}`,
+            }).then(() => {
+                if(howToStartMessage) {
+                    console.log(chalk.bgBlack.cyan('Done!'));
+                    console.log('');
+                    console.log(chalk.bgBlack.white('To get started:'));
+                    console.log(chalk.bgBlack.cyan(`cd ${dirName}`));
+                    console.log(chalk.bgBlack.cyan(howToStartMessage));
+                }
+            });
+        });
+    }
+
+    if(repoName === 'fullstack') {
+        cloneRepo(`${dirName}/backend`, 'JeriRov/express-ts-template.git', undefined);
+        cloneRepo(`${dirName}/frontend`, 'JeriRov/react-ts-template.git', howToStartMessage);
+        return;
+    }
+    cloneRepo(dirName, repoName, howToStartMessage);
 });
